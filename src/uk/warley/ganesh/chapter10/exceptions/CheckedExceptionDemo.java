cf3ec38dd9b63a83e45514bea9a01c10d90a70f3
ganeshtidke0901 <ganeshtidke0901@gmail.com>
2021-08-22 20:18:47 +0100

    Added class 10 Exceptions

---
+++ src/uk/warley/ganesh/chapter10/exceptions/CheckedExceptionDemo.java
@@ -0,0 +1,120 @@
+package uk.warley.ganesh.chapter10.exceptions;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class CheckedExceptionDemo {
+	public static void main(String[] args) {
+		try {
+			example6();
+		} catch (IOException e3) {
+			// TODO Auto-generated catch block
+			e3.printStackTrace();
+		}
+		example5();
+		try {
+			example4();
+		} catch (Throwable e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		example3();
+		try {
+			example2();
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			example1();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	private static void example1() throws Exception {
+		// if it declares checked exception ten caller has to have handler even this method does not throw
+		// exception
+
+	}
+
+	private static void example2() throws Exception {
+		try {
+
+		} catch (Exception e) {
+
+		} finally {
+			throw new Exception();
+		}
+	}
+
+	private static void example3()  {
+		try {
+			System.out.println("something");
+		} catch (Throwable e) {
+
+		}
+	}
+
+	private static void example4() throws Throwable {
+
+		try {
+			System.out.println("something");
+			throw new Exception();
+		} catch (Exception e) {
+			throw new Throwable();
+		}
+	}
+
+	private static void example5() {
+		try {
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		// IOException is never throw so hadlder is never reachable
+//		try {
+//
+//		} catch (IOException e) {
+//			// TODO: handle exception
+//		}
+
+//		try {
+//
+//		} catch (AbsentInformationException e) {
+//			// TODO: handle exception
+//		}
+
+//		try {
+//			
+//		} catch (RandomException e) {
+//
+//		}
+
+		try {
+			throw new IOException();
+		} catch (IOException e) {
+			// TODO: handle exception
+		}
+		
+//		try {
+//		} catch (BrokenBarrierException e) {
+//			// TODO: handle exception
+//		}
+	}
+	
+	private static void example6() throws IOException  {
+			System.out.println("something");
+	}
+	
+	private static void example7() throws RuntimeException{
+		System.out.println("something");
+}
+
+
+	static class RandomException extends Exception {
+	}
+}
