2393caa145cadfd915a64c91eab056568a6740c5
ganeshtidke0901 <ganeshtidke0901@gmail.com>
2021-09-25 12:13:55 +0100

    Added classes

---
+++ src/uk/warley/ganesh/chapter17/concurrency/AtomicExample.java
@@ -0,0 +1,9 @@
+package uk.warley.ganesh.chapter17.concurrency;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicExample {
+	public static void main(String[] args) {
+		AtomicInteger atomicInteger=new AtomicInteger();
+	}
+}
